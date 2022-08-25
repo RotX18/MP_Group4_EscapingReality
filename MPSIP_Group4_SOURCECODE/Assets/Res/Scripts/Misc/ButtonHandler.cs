2ee98a4f778928c852eeea1be0e5e17c3b46a730
RabbitKazma <85175456+RabbitKazma@users.noreply.github.com>
2022-08-25 16:16:25 +0800

    Further changes

--- MPSIP_Group4_SOURCECODE/Assets/Res/Scripts/Misc/ButtonHandler.cs
+++ MPSIP_Group4_SOURCECODE/Assets/Res/Scripts/Misc/ButtonHandler.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class ButtonHandler : MonoBehaviour
@@ -10,46 +11,38 @@ public class ButtonHandler : MonoBehaviour
     private Ray _pointerRay;
     private RaycastHit _hit;
     private GameObject _hitObj;
-
-    private bool _rIndexTrigger;
+    private bool _doRaycast = false;
 
     private void Update()
     {
-        _rIndexTrigger = OVRInput.GetDown(OVRInput.RawButton.RIndexTrigger);
-
-        if (_rIndexTrigger)
-        {
-            if (_hitObj == null) return;
-            //check if it collides with the gameobject name StartButton
-            if (_hitObj.GetComponent<Collider>().name.Equals("StartButton"))
-            {
-                //loadScene
-                SceneManager.LoadScene(1);
-                Debug.Log("loadscene");
-            }
-            else if (_hitObj.GetComponent<Collider>().name.Equals("ReturnButton"))
-            {
-                //loadScene
-                SceneManager.LoadScene(0);
-            }
-            else return;
+        if (OVRInput.GetDown(OVRInput.RawButton.RIndexTrigger)){
+            //if the right index trigger is pressed, set hit obj to null and do raycast
+            _hitObj = null;
+            _doRaycast = true;
+        }
+        else {
+            _doRaycast = false;
         }
 
+        //hit obj handling
+        if (_hitObj != null && _hitObj.GetComponentInChildren<Button>() != null) {
+            char[] remove = { 'b', 't', 'n' };
+            SceneManager.LoadScene(_hit.collider.name.TrimStart(remove));
+        }
     }
 
     private void FixedUpdate()
     {
-        //casting from the main camera to the point on screen where the mouse is
-        _pointerRay = new Ray(pointerOrigin.transform.position, pointerOrigin.transform.forward);
+        if (_doRaycast)
+        {
+            //casting from the main camera to the point on screen where the mouse is
+            _pointerRay = new Ray(pointerOrigin.transform.position, pointerOrigin.transform.forward);
 
-        if (Physics.Raycast(_pointerRay, out _hit))
-        {
-            //if the point where the player click contains a gameobject
-            _hitObj = _hit.transform.gameObject;
-        }
-        else
-        {
-            _hitObj = null;
+            if (Physics.Raycast(_pointerRay, out _hit))
+            {
+                //if the point where the player click contains a gameobject
+                _hitObj = _hit.transform.gameObject;
+            }
         }
     }
 }
