4135547e3e30fb25446f1484ba0b5db98a667926
RabbitKazma <85175456+RabbitKazma@users.noreply.github.com>
2022-09-05 20:11:30 +0800

    added UI animation
    - basically for scripting part in game manger when u press button it plays animation instead of setactive false or true

--- MPSIP_Group4_SOURCECODE/Assets/Res/Scripts/Misc/GameManager.cs
+++ MPSIP_Group4_SOURCECODE/Assets/Res/Scripts/Misc/GameManager.cs
@@ -12,6 +12,7 @@ public class GameManager : MonoBehaviour
     public GameObject[] puzzles;
 
     //UI
+    public Animator UIanimation;
     public GameObject canvasUI;
     public GameObject canvasHint;
     #endregion
@@ -38,17 +39,12 @@ public class GameManager : MonoBehaviour
         }
         
         //hiding/unhiding the ui
-        if(OVRInput.GetDown(OVRInput.RawButton.X)){ 
-            if(canvasUI.activeInHierarchy){
-                //if the canvasUI is active, set it to inactive
-                canvasUI.SetActive(false);
-            }
-            else{
-                //else set it to active
-                canvasUI.SetActive(true);
-            }
+        if(OVRInput.GetDown(OVRInput.RawButton.Y)){
+
+            UIanimation.SetTrigger("ButtonTrigger");
+
         }
-        if(OVRInput.GetDown(OVRInput.RawButton.Y)){ 
+        if(OVRInput.GetDown(OVRInput.RawButton.X)){ 
             if(canvasHint.activeInHierarchy){
                 //if the canvasHint is active, set it to inactive
                 canvasHint.SetActive(false);
